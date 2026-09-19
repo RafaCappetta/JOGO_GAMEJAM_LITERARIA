7dd3f632752ca8031b66672f546269d1a697b1b0
Ian Felipe <ian.felipe.drunen@gmail.com>
2026-09-19 17:36:58 -0300

    Adicionando base do sistema de pegar item

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,8 +7,14 @@ const SENSIBILIDADE = 0.003
 @onready var cabeca : Node3D = $"Cabeça"
 @onready var camera = $"Cabeça/Camera3D"
 
+var pode_pegar_item = false
+var item : Node3D = null
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _process(delta: float) -> void:
+	pegar_item()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -37,3 +43,20 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Item"):
+		pode_pegar_item = true
+		item = body
+		
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("Item"):
+		item = null
+		pode_pegar_item = false
+		
+func pegar_item():
+	if pode_pegar_item == true and Input.is_action_just_pressed("pegar"):
+		if item != null:
+			item.foi_pego()
+			print("Pegou")
+			pode_pegar_item = false
